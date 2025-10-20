39907ba94db331a617c049c41a039e72db4e6cfd
Khan-razaq <khan.razaqma@gmail.com>
2025-10-20 01:13:13 -0400

    Added grey filter to video diplay

--- PRCV/vidDisplay.cpp
+++ PRCV/vidDisplay.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+bool grayFlag;
+
 int main(){
     cv::VideoCapture cap(0);
     if(!cap.isOpened()){
@@ -11,6 +13,8 @@ int main(){
     }
     cv::Mat frame;
     cv::Mat flipped_frame;
+    cv::Mat gray_frame;
+    cv::Mat display_frame;
     
     cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
 	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
@@ -19,22 +23,36 @@ int main(){
     while(true){
     	cap >> frame;
     	if(frame.empty()){
-        cerr << "Error: Empty frame" << endl;
-        break;
+		    cerr << "Error: Empty frame" << endl;
+		    break;
 		}
 		
-		cv::flip(frame, flipped_frame, 1); 
-    	cv::imshow("Video Window", flipped_frame);
+		cv::flip(frame, flipped_frame, 1);
+		if(grayFlag){
+			cvtColor(flipped_frame, gray_frame, cv::COLOR_RGB2GRAY);
+			display_frame = gray_frame;
+		}
+		else{
+			display_frame = flipped_frame;
+    	}
+    	cv::imshow("Video Window", display_frame);
+    	
     	char key = cv::waitKey(30);
     	
     	if(key == 'q'){
+    		cout << "Quitting" << endl;
     		break;
     	}
     	else if(key == 's'){
-    		cv::imwrite("screenshot.jpg", flipped_frame);
-    		cout << "Image saved!" << endl;
+    		cv::imwrite("screenshot.jpg", display_frame);
+    		cout << "Screenshot taken!" << endl;
     	}
-
+    	else if(key == 'g'){
+    		grayFlag = true;
+    	}
+    	else if(key == 'o'){
+    		grayFlag = false;
+    	}
     }
     cap.release();
     cv::destroyAllWindows();
